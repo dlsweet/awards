1be4167902ba0b14b8b851f6e86bf9a5610b95ef
carneybill <bill@rstudio.com>
2017-10-02 09:24:59 -0400

    Curtis_code

---
+++ Curtis _code.R
@@ -0,0 +1,73 @@
+library(httr)
+library(dplyr)
+
+
+url_users <- function(page = 1, domain = "https://community.rstudio.com"){
+  paste(
+    domain,
+    
+    "/directory_items.json?&page=",
+    page,
+    "&period=all&order=likes_received",
+    sep = ""
+  )
+}
+
+url <- url_users()
+
+req <- httr::GET(url)
+stop_for_status(req)
+con <- httr::content(req)
+
+users <- data.frame() %>% tbl_df
+user_cnt = 0
+page = 1
+
+while (length(con$directory_items) > 0){
+  
+  users_raw <- con$directory_items
+  
+  users = bind_rows(
+    users,
+    tibble(
+      id = users_raw %>% map_int(c("user", "id")),
+      name = users_raw %>% map_chr(c("user", "username")),
+      likes = users_raw %>% map_int("likes_received"),
+      title = users_raw %>% map_chr(c("user", "title"), .default = NA),
+      time_read = users_raw %>% map_chr(c("time_read"), .default = NA),
+      likes_given = users_raw %>% map_chr(c("likes_given"), .default = NA),
+      topics_entered = users_raw %>% map_chr(c("topics_entered"), .default = NA),
+      post_count = users_raw %>% map_chr(c("post_count"), .default = NA),
+      days_visited = users_raw %>% map_chr(c("days_visited"), .default = NA)
+    )
+  )
+  
+  
+  
+  page = page + 1
+  req <- httr::GET(url_users(page))
+  stop_for_status(req)
+  con <- httr::content(req)
+  
+  user_cnt = users %>% nrow
+  print(paste(
+    "Page:", page, 
+    "- users:", user_cnt
+  ))
+}
+
+users = users %>%
+  distinct(name, .keep_all = TRUE) %>%
+  mutate(
+    DateTime = Sys.time()
+  )
+
+winners <- c(
+  "mara", "alistaire", "daattali", "emilyriederer", "eric_bickel", "nick", "jessemaegan", "raybuhr", "billr", "mmuurr"
+)
+
+users %>%
+  filter(is.na(title), !name %in% winners) %>%
+  select(-title) %>%
+  sample_n(5, weight = likes) %>%
+  arrange(desc(likes))
